ab69d545f3300ec5f24d470e03872b244bb4c173
joseph0x45 <zozozozeph@gmail.com>
2026-01-15 07:48:27 +0000

    update seed

--- seed.sql
+++ seed.sql
@@ -1,11 +1,11 @@
-INSERT INTO tasks (id, label, status, due_date, owner_id) VALUES 
-('task1', 'Task 1', 'Pending', '2026-01-10', '3CxV0RVvR'),
-('task2', 'Task 2', 'Pending', '2026-01-12', '3CxV0RVvR'),
-('task3', 'Task 3', 'In Progress', '2026-01-14', '3CxV0RVvR'),
-('task4', 'Task 4', 'Completed', '2026-01-01', '3CxV0RVvR'),
-('task5', 'Task 5', 'Pending', '2026-01-15', '3CxV0RVvR'),
-('task6', 'Task 6', 'Pending', '2026-01-20', '3CxV0RVvR'),
-('task7', 'Task 7', 'In Progress', '2026-01-05', '3CxV0RVvR'),
-('task8', 'Task 8', 'Completed', '2026-01-02', '3CxV0RVvR'),
-('task9', 'Task 9', 'Pending', '2026-01-25', '3CxV0RVvR'),
-('task10', 'Task 10', 'Pending', '2026-01-30', '3CxV0RVvR');
+INSERT INTO tasks (id, label, status, due_date) VALUES 
+('task1', 'Task 1', 'Pending', '2026-01-10'),
+('task2', 'Task 2', 'Pending', '2026-01-12'),
+('task3', 'Task 3', 'In Progress', '2026-01-14'),
+('task4', 'Task 4', 'Completed', '2026-01-01'),
+('task5', 'Task 5', 'Pending', '2026-01-15'),
+('task6', 'Task 6', 'Pending', '2026-01-20'),
+('task7', 'Task 7', 'In Progress', '2026-01-05'),
+('task8', 'Task 8', 'Completed', '2026-01-02'),
+('task9', 'Task 9', 'Pending', '2026-01-25'),
+('task10', 'Task 10', 'Pending', '2026-01-30');
